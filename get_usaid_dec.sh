806139d7ffc4633bd3ccd282364c590f7027ee7f
Albert Decatur <adecatur@aiddata.org>
2014-09-29 12:32:14 -0400

    use curl directly. remove newline from base64

--- get_usaid_dec.sh
+++ get_usaid_dec.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # use qsearch on USAID site to get CSV of metadata for recent evaluation docs
+# user args: 1) USAID qsearch string to query.  NB: user must escape quotes as necessary
+# example use: $0 '(documents.web_collection:("recent evaluations"))'
 
 # make function to url encode
 function url_encode { perl -MURI::Escape -e 'print uri_escape($ARGV[0]); print "\n";' $1 ;}
 baseurl="https://dec.usaid.gov/api/qsearch.ashx?q="
-search='(documents.web_collection:("recent evaluations"))'
-base64=$( echo -e "$search\c" | base64  )
+search=$1
+base64=$( echo -e "$search\c" | base64  | tr -d '\n' )
 qstring=$( url_encode "$base64" )
-# written to pipe to sh so we can easily check URL
-echo curl -s "${baseurl}${qstring}&rtype=CSV" | sh
+curl -s "${baseurl}${qstring}&rtype=CSV"
